d3a65b81b70a863b7647e2d62bdaf49d3a6a2c5e
Oscar <oscaradrian.ia@gmail.com>
2020-11-26 00:49:31 -0600

    primer commit

---
+++ config.js
@@ -0,0 +1,18 @@
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+      apiKey: "AIzaSyCIT8P3dkMxJmJD3tha_j8sRjdB79ugWuI",
+      authDomain: "pruebas-login-erunetha.firebaseapp.com",
+      databaseURL: "https://pruebas-login-erunetha.firebaseio.com",
+      projectId: "pruebas-login-erunetha",
+      storageBucket: "pruebas-login-erunetha.appspot.com",
+      messagingSenderId: "1058140092736",
+      appId: "1:1058140092736:web:88f7440db202301a78e2a5",
+      measurementId: "G-YB4X1Z4Y93"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+
+  var CLIENT_ID = null;
